af0653ba1e0eac43bb8162fd94c91dd99f3c6d14
Spencer Powell <krornus@gmail.com>
2022-08-08 13:13:07 -0600

    base layout + containerizing

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,38 @@
 use crate::rect::Rect;
 use crate::tag::TagIndex;
+use crate::wm::Adapter;
 
 use xcb::x;
 
 pub struct Client {
-    pub scope: Rect,
     pub window: x::Window,
     pub visible: bool,
-    index: TagIndex,
+    pub focus: bool,
+    pub index: TagIndex,
+    pub rect: Rect,
+}
+
+impl Client {
+    pub fn focus(&mut self, p: bool) {
+        if self.focus != p {
+            /* TODO: actually grab/release focus */
+            self.focus = p;
+        }
+    }
+
+    pub fn resize(&mut self, adapter: &mut Adapter, rect: Rect) {
+        if self.rect != rect {
+            self.rect = rect;
+
+            adapter.request(&x::ConfigureWindow {
+                window: self.window,
+                value_list: &[
+                    x::ConfigWindow::X(self.rect.x as i32),
+                    x::ConfigWindow::Y(self.rect.y as i32),
+                    x::ConfigWindow::Width(self.rect.w as u32),
+                    x::ConfigWindow::Height(self.rect.h as u32),
+                ],
+            });
+        }
+    }
 }
--- src/container.rs
+++ src/container.rs
@@ -1,90 +1,145 @@
-use std::collections::VecDeque;
-
 use crate::rect::{Rect, Contains};
 use crate::client::Client;
 
 use xcb::x;
 
-pub struct ContainerIterator<'a> {
-    containers: Vec<&'a Container>,
+pub enum ContainerNode {
+    Container(Container),
+    Client(Client),
 }
 
-impl<'a> Iterator for ContainerIterator<'a> {
-    type Item = &'a Container;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let con = self.containers.pop()?;
-
-        self.containers.reserve(con.children.len());
-        for child in con.children.iter() {
-            self.containers.push(child);
+impl ContainerNode {
+    fn into_container(self) -> Container {
+        match self {
+            ContainerNode::Container(c) => c,
+            ContainerNode::Client(_) => panic!("into_container() on Client variant"),
         }
-
-        Some(con)
     }
-}
 
-pub struct ClientIterator<'a> {
-    clients: std::slice::Iter<'a, Client>,
-    containers: ContainerIterator<'a>,
-}
-
-impl<'a> Iterator for ClientIterator<'a> {
-    type Item = &'a Client;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(x) = self.clients.next() {
-            return Some(x);
+    fn into_client(self) -> Client {
+        match self {
+            ContainerNode::Client(c) => c,
+            ContainerNode::Container(_) => panic!("into_client() on Container variant"),
         }
-
-        let con = self.containers.next()?;
-        self.clients = con.stack.iter();
-        self.next()
     }
 }
 
 pub struct Container {
-    rect: Rect,
-    stack: Vec<Client>,
-    active: Option<usize>,
-    children: Vec<Container>,
+    pub rect: Rect,
+    focus: Option<usize>,
+    children: Vec<ContainerNode>,
 }
 
 impl Container {
-    pub fn new(rect: Rect, stack: Vec<Client>) -> Self {
+    pub fn new(rect: Rect) -> Self {
         Container {
             rect: rect,
-            stack: stack,
-            active: None,
+            focus: None,
             children: vec![],
         }
     }
 
-    pub fn add(&mut self, con: Container) {
-        assert!(self.rect.contains(&con.rect));
-        self.children.push(con)
+    pub fn scope(&mut self, rect: Rect) {
+       assert!(self.rect.contains(&rect));
+        let con = ContainerNode::Container(Self::new(rect));
+        self.children.push(con);
     }
 
-    /// get an iterator of all containers
-    pub fn containers(&self) -> ContainerIterator<'_> {
-        ContainerIterator {
-            containers: vec![self]
+    pub fn client(&mut self, client: Client) {
+        let con = ContainerNode::Client(client);
+        self.children.push(con);
+    }
+
+    #[inline]
+    pub fn focus(&mut self, window: x::Window) {
+        self.focus_client(window);
+    }
+
+    pub fn remove(&mut self, window: x::Window) -> Option<Client> {
+        let mut index = None;
+
+        for (i, con) in self.children.iter_mut().enumerate() {
+            match con {
+                ContainerNode::Client(c) => {
+                    if c.window == window {
+                        index = Some(i);
+                        break;
+                    }
+                },
+                ContainerNode::Container(s) => {
+                    let client = s.remove(window);
+                    if client.is_some() {
+                        return client;
+                    }
+                },
+            }
+        }
+
+        if let Some(i) = index {
+            let con = self.children.remove(i);
+            Some(con.into_client())
+        } else {
+            None
         }
     }
 
-    /// get an iterator of all clients
-    pub fn clients(&self) -> ClientIterator<'_> {
-        let mut containers = self.containers();
-        containers.next().unwrap();
-
-        ClientIterator {
-            clients: self.stack.iter(),
-            containers: containers,
-        }
+    pub fn merge(&mut self, other: Container) {
+        /* TODO: for when monitor is disconnected */
     }
 
-    /// get a mutable reference to a client based on its window
-    pub fn client(&self, window: x::Window) -> Option<&Client> {
-        self.clients().find(|c| &c.window == &window)
+    pub fn clients_mut(&mut self, recursive: bool) -> Vec<&mut Client> {
+        /* this is not an iterator on purpose. it is used for layouts,
+         * which must know the total number of clients before
+         * the rendering process begins. */
+        let mut clients = Vec::with_capacity(self.children.len());
+
+        for x in self.children.iter_mut() {
+            match x {
+                ContainerNode::Container(c) => {
+                    if recursive {
+                        let mut refs = c.clients_mut(recursive);
+                        clients.append(&mut refs);
+                    }
+                },
+                ContainerNode::Client(c) => {
+                    clients.push(c);
+                }
+            }
+        }
+
+        clients
+    }
+}
+
+impl Container {
+    fn focus_client(&mut self, window: x::Window) -> bool {
+        let mut focus = None;
+
+        /* dont break loop early, we need to reset any previously focused items to false */
+        for (i, con) in self.children.iter_mut().enumerate() {
+            match con {
+                ContainerNode::Client(c) => {
+                    if c.window == window {
+                        focus = Some(i);
+                        c.focus(true);
+                    } else {
+                        c.focus(false);
+                    }
+                },
+                ContainerNode::Container(s) => {
+                    if s.focus_client(window) {
+                        focus = Some(i);
+                    }
+                },
+            }
+        }
+
+        if focus.is_some() {
+            self.focus = focus;
+            true
+        } else {
+            self.focus = None;
+            false
+        }
     }
 }
